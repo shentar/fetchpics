a6e999f3dd4969b8284f249392020576b082b35f
Tongyanqun <tongyanqun@bytedance.com>
2022-03-05 19:53:35 +0800

    supports telegram channel.

--- getpic.go
+++ getpic.go
@@ -307,6 +307,7 @@ func dealWithOneUrl(user *OneUser) {
 	)
 	s := time.Now()
 	parser := gofeed.NewParser()
+	parser.Client = user.client
 	parser.UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.71 Safari/537.36"
 	feedUrl := rsshubUrl + seed
 	feed, err := parser.ParseURL(feedUrl)
